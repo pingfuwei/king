85eecb92af4e63b869cd09f0c41c9d033a3fd77f
“haizhe <2238505435@qq.com>
2020-07-21 20:08:12 +0800

    xu

---
+++ app/Http/Controllers/admin/StockController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\AdminModel\Goods_attrModel;
+use App\AdminModel\GoodsvalueModel;
+use App\AdminModel\goods_stock;
+use Illuminate\Support\Facades\DB;
+class StockController extends Controller
+{
+    public function create(){
+        $res = db::table('shop_goods')->get();
+        $data = Goods_attrModel::get();
+        foreach ($data as $k=>$v){
+            $ret = GoodsvalueModel::where('attr_id',$v['attr_id'])->get();
+            $v['data'] = $ret;
+        }
+        return view('admin.stock.create',['res'=>$res,'data'=>$data]);
+    }
+    public function createDo(){
+
+    }
+    public function index(){
+
+    }
+    public function del(){
+
+    }
+    public function upd(){
+
+    }
+    public function updDo(){
+
+    }
+}
