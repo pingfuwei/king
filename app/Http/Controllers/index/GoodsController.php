46c1966445f990cee31988b8899254224758f2b2
yahao <1743398384@qq.com>
2020-07-29 17:05:55 +0800

    add

--- app/Http/Controllers/index/GoodsController.php
+++ app/Http/Controllers/index/GoodsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\index;
 
 use App\AdminModel\Goods;
+use App\AdminModel\goods_stock;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -35,7 +36,39 @@ class GoodsController extends Controller
             ];
             return json_encode($message,JSON_UNESCAPED_UNICODE);
         }else{
+            $stock_model=new goods_stock();
+            $where=[
+                ['goods_id','=',$goods_info['goods_id']],
+                ['is_del','=',1]
+            ];
+            $goods_stock=$stock_model::where($where)->get()->toArray();
+            foreach($goods_stock as $k=>$v){
+                $ass=explode(':', $v['ability']);
+                foreach($ass as $kk=>$vv){
+                    $asd=explode(',', $v['ability']);
+                }
+            }
+//            print_r($goods_stock);die;
             return view('index.goods.desc',['goods'=>$goods_info]);
+        }
+    }
+    function cut_str($str,$sign,$number){
+        $array=explode($sign, $str);
+        $length=count($array);
+        if($number<0){
+            $new_array=array_reverse($array);
+            $abs_number=abs($number);
+            if($abs_number>$length){
+                return 'error';
+            }else{
+                return $new_array[$abs_number-1];
+            }
+        }else{
+            if($number>=$length){
+                return 'error';
+            }else{
+                return $array[$number];
+            }
         }
     }
 }
